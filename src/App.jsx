1e966d67cab2442999dc5c75898b1675d7d0210b
firas hamila <firas.hm.1996@gmail.com>
2025-01-22 20:36:40 +0100

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: "Item 1",
+      price: 1111,
+      date: new Date(2025, 1, 22),
+    },
+    {
+      id: 2,
+      title: "Item 2",
+      price: 2222,
+      date: new Date(2025, 10, 15),
+    },
+    {
+      id: 3,
+      title: "Item 3",
+      price: 3333,
+      date: new Date(2024, 5, 1),
+    },
+    {
+      id: 4,
+      title: "Item 4",
+      price: 4444,
+      date: new Date(2023, 8, 20),
+    },
+  ];
   return (
     <div>
-      <h1>Lets get started !!!</h1>
+      {data.map((item) => {
+        return (
+          <ExpenseItem title={item.title} date={item.date} price={item.price} />
+        );
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,15 @@
-const ExpenseItem = () => {
-  return <div></div>;
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+      <div className="expense-date">date</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">price</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
