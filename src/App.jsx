9179ef7903992258c456d3448c6bac9da3227218
firas hamila <firas.hm.1996@gmail.com>
2025-02-12 20:24:54 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AddExpenseForm from "./components/AddExpenseForm";
 import ExpenseItem from "./components/ExpenseItem";
 import ExpensesContainer from "./components/ExpensesContainer";
 
@@ -36,6 +37,7 @@ function App() {
   ];
   return (
     <div>
+      <AddExpenseForm />
       <ExpensesContainer data={data} />
     </div>
   );
