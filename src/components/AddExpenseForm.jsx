c038ffad6cdf9f0b0bebe6f060875ea216049eec
firas hamila <firas.hm.1996@gmail.com>
2025-02-19 19:27:50 +0100

    a

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,29 +1,63 @@
+import { useState } from "react";
 import "./AddExpenseForm.css";
 
 const AddExpenseForm = () => {
+  const [inputs, setInputs] = useState({
+    title: "",
+    price: "",
+    date: "",
+  });
+  const inputsHandler = ({ target }) => {
+    const { name, value } = target;
+    setInputs((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
   const myDate = new Date(Date.now()).getFullYear();
   const minDate = `${myDate - 2}-01-01`;
   const maxDate = `${myDate + 2}-12-31`;
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    setInputs({
+      title: "",
+      price: "",
+      date: "",
+    });
+  };
   return (
     <div className="new-expense">
-      <form>
+      <form onSubmit={submitForm}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input placeholder="Title" />
+            <input onChange={inputsHandler} name="title" placeholder="Title" />
           </div>
           <div className="new-expense__control">
             <label>Price</label>
-            <input placeholder="Price" type="number" min="0" step="0.01" />
+            <input
+              onChange={inputsHandler}
+              name="price"
+              placeholder="Price"
+              type="number"
+              min="0"
+              step="0.01"
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min={minDate} max={maxDate} />
+            <input
+              onChange={inputsHandler}
+              name="date"
+              type="date"
+              min={minDate}
+              max={maxDate}
+            />
           </div>
         </div>
         <div className="new-expense__actions">
           <button>Cancel</button>
-          <button>Add Expense</button>
+          <button type="submit">Add Expense</button>
         </div>
       </form>
     </div>
